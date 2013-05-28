d508af494e714bec13fad4970485aedbc1e6953d
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 16:34:45 -0400

    only for rails

--- recipes/prepare.rb
+++ recipes/prepare.rb
@@ -1,5 +1,9 @@
 node[:deploy].each do |application, deploy|
   Chef::Log.info("Ensuring shared/assets directory for #{application} app...")
+	 if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping deploy::rails application #{application} as it is not an Rails app")
+    next
+  end
  
   directory "#{deploy[:deploy_to]}/shared/assets" do
     group deploy[:group]
